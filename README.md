54ea9a9c59f63b6dc681dfde2a4968eb045f3555
Tom Paulus <tom@tompaulus.com>
2024-05-10 20:50:45 -0700

    Add Worker Logic

---
+++ README.md
@@ -0,0 +1,4 @@
+Github Argo Proxy
+=================
+
+A simple Cloudflare Worker that accepts Github Webhooks and forwards them to ArgoCD which is protected by Access.
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,64 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Buffer } from 'node:buffer';
+import * as node_crypto from "node:crypto";
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+		if (request.method !== "POST") return MethodNotAllowed(request);
+
+		const signatureHeader: string | null = request.headers.get("x-hub-signature-256");
+
+		if (!signatureHeader) {
+			return new Response("Missing Request Signature", 
+				{
+					status: 401
+				}
+			);
+		}
+		
+		if (!await verifySignature(
+			env.WEBHOOK_SECRET,
+			await request.clone().text(),
+			signatureHeader
+		)) {
+			return new Response("Invalid Request Signature", 
+				{
+					status: 403
+				}
+			);
+		}
+
+		const upstreamRequest = new Request(
+			env.ARGO_URL,
+			new Request(request, {
+				headers: {
+					...request.headers,
+					"CF-Access-Client-Id": env.ACCESS_CLIENT_ID,
+					"CF-Access-Client-Secret": env.ACCESS_CLIENT_SECRET,
+				}
+			})
+		)
+
+		// @ts-ignore
+		return fetch(upstreamRequest);
+	}
+} satisfies ExportedHandler<Env>;
+
+function MethodNotAllowed(request: Request): Response {
+	return new Response(`Method ${request.method} not allowed.`, {
+		status: 405,
+		headers: {
+		Allow: "GET",
+		},
+	});
+}
+
+async function verifySignature(secret: string, payload: string, header: string) {
+	const signature = node_crypto
+		.createHmac("sha256", secret)
+		.update(payload)
+		.digest("hex");
+	let trusted = Buffer.from(`sha256=${signature}`, 'ascii');
+
+	let untrusted = Buffer.from(header, 'ascii');
+	return crypto.subtle.timingSafeEqual(trusted, untrusted);
 };
